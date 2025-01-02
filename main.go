ab92c4bf30ba1b35857eef8cb05c7eb7696a1b19
SooJae <developersoojae@gmail.com>
2025-01-02 14:41:14 +0900

    feat: add viper library

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"github.com/soojae/simplebank/util"
 	"log"
 
 	_ "github.com/lib/pq"
@@ -9,14 +10,13 @@ import (
 	db "github.com/soojae/simplebank/db/sqlc"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgres://your_username:your_password@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db", err)
 	}
@@ -24,7 +24,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
