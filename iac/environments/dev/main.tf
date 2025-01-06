f59df3d3f86d5c684b6140ed8a9fbb17e9a80cf9
SooJae <developersoojae@gmail.com>
2025-01-06 15:20:17 +0900

    chore: deploy worflow: build and push docker image to ECR

---
+++ iac/environments/dev/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = "~> 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "container_registry" {
+  source = "../../modules/container-registry"
+
+  ecr_repository_name = var.ecr_repository_name
+  ecr_image_tag_mutability = var.ecr_image_tag_mutability
+  ecr_scan_on_push = var.ecr_scan_on_push
+  ecr_encryption_type = var.ecr_encryption_type
+}
