e32160effadcf3b055c02642e7449ac509cd7cfa
SooJae <developersoojae@gmail.com>
2025-01-13 18:37:35 +0900

    feat: add validation for user login and creation requests

--- main.go
+++ main.go
@@ -32,9 +32,9 @@ func main() {
 	}
 
 	store := db.NewStore(conn)
-	//go runGatewayServer(config, store)
-	//runGrpcServer(config, store)
-	runGinServer(config, store)
+	go runGatewayServer(config, store)
+	runGrpcServer(config, store)
+	//runGinServer(config, store)
 
 }
 
