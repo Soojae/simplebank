17977d60ebf194fe38d170a0466edc4b0745a29b
SooJae <developersoojae@gmail.com>
2025-01-18 03:34:07 +0900

    feat[#21]: implement user authorization and enhance UpdateUser method with access control
    - Added authorization logic in a new `authorization.go` file to validate user access tokens.
    - Updated `UpdateUser` method to include authorization checks, ensuring users can only update their own information.
    - Introduced `unauthorizedError` function in `error.go` for consistent error handling related to authentication failures.

--- gapi/error.go
+++ gapi/error.go
@@ -23,3 +23,7 @@ func invalidArgumentError(violations []*errdetails.BadRequest_FieldViolation) er
 	}
 	return statusDetails.Err()
 }
+
+func unauthorizedError(err error) error {
+	return status.Errorf(codes.Unauthenticated, "unauthorized: %s", err)
+}
